b2c4ea38dfe39bff3caa435a1ccfec07747d7639
Enes Alili <enesalili00@gmail.com>
2018-05-13 19:31:19 +0200

    Changes to the design

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -41,22 +41,25 @@
             // 
             // titleLabel
             // 
-            this.titleLabel.AutoSize = true;
-            this.titleLabel.Font = new System.Drawing.Font("Constantia", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.Font = new System.Drawing.Font("Tempus Sans ITC", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.titleLabel.ForeColor = System.Drawing.Color.White;
-            this.titleLabel.Location = new System.Drawing.Point(432, 45);
+            this.titleLabel.Location = new System.Drawing.Point(0, 26);
+            this.titleLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.titleLabel.Name = "titleLabel";
-            this.titleLabel.Size = new System.Drawing.Size(238, 29);
+            this.titleLabel.Size = new System.Drawing.Size(812, 53);
             this.titleLabel.TabIndex = 0;
             this.titleLabel.Text = "MathProblemSolver";
             this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // shapeChoosingLabel
             // 
-            this.shapeChoosingLabel.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.shapeChoosingLabel.Location = new System.Drawing.Point(1, 184);
+            this.shapeChoosingLabel.BackColor = System.Drawing.Color.Snow;
+            this.shapeChoosingLabel.Font = new System.Drawing.Font("Constantia", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.shapeChoosingLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.shapeChoosingLabel.Location = new System.Drawing.Point(1, 130);
+            this.shapeChoosingLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.shapeChoosingLabel.Name = "shapeChoosingLabel";
-            this.shapeChoosingLabel.Size = new System.Drawing.Size(1083, 24);
+            this.shapeChoosingLabel.Size = new System.Drawing.Size(812, 34);
             this.shapeChoosingLabel.TabIndex = 1;
             this.shapeChoosingLabel.Text = "Choose the type below";
             this.shapeChoosingLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -72,7 +75,8 @@
             this.twoDimensionalButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
             this.twoDimensionalButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.twoDimensionalButton.Image = ((System.Drawing.Image)(resources.GetObject("twoDimensionalButton.Image")));
-            this.twoDimensionalButton.Location = new System.Drawing.Point(354, 275);
+            this.twoDimensionalButton.Location = new System.Drawing.Point(241, 223);
+            this.twoDimensionalButton.Margin = new System.Windows.Forms.Padding(2);
             this.twoDimensionalButton.Name = "twoDimensionalButton";
             this.twoDimensionalButton.Size = new System.Drawing.Size(146, 146);
             this.twoDimensionalButton.TabIndex = 2;
@@ -82,37 +86,44 @@
             // 
             // headerPanel
             // 
-            this.headerPanel.BackColor = System.Drawing.Color.DimGray;
+            this.headerPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
             this.headerPanel.Controls.Add(this.titleLabel);
             this.headerPanel.Location = new System.Drawing.Point(1, 0);
+            this.headerPanel.Margin = new System.Windows.Forms.Padding(2);
             this.headerPanel.Name = "headerPanel";
-            this.headerPanel.Size = new System.Drawing.Size(1083, 120);
+            this.headerPanel.Size = new System.Drawing.Size(812, 98);
             this.headerPanel.TabIndex = 4;
             // 
             // threeDimensionalButton
             // 
             this.threeDimensionalButton.AutoSize = true;
+            this.threeDimensionalButton.BackColor = System.Drawing.Color.Transparent;
+            this.threeDimensionalButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.threeDimensionalButton.Cursor = System.Windows.Forms.Cursors.Hand;
             this.threeDimensionalButton.FlatAppearance.BorderColor = System.Drawing.Color.DeepSkyBlue;
             this.threeDimensionalButton.FlatAppearance.BorderSize = 0;
             this.threeDimensionalButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
             this.threeDimensionalButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.threeDimensionalButton.Image = ((System.Drawing.Image)(resources.GetObject("threeDimensionalButton.Image")));
-            this.threeDimensionalButton.Location = new System.Drawing.Point(554, 275);
+            this.threeDimensionalButton.Location = new System.Drawing.Point(438, 223);
+            this.threeDimensionalButton.Margin = new System.Windows.Forms.Padding(2);
             this.threeDimensionalButton.Name = "threeDimensionalButton";
             this.threeDimensionalButton.Size = new System.Drawing.Size(146, 146);
             this.threeDimensionalButton.TabIndex = 3;
-            this.threeDimensionalButton.UseVisualStyleBackColor = true;
+            this.threeDimensionalButton.UseVisualStyleBackColor = false;
             this.threeDimensionalButton.MouseLeave += new System.EventHandler(this.threeDimensionalButton_MouseLeave);
             this.threeDimensionalButton.MouseHover += new System.EventHandler(this.threeDimensionalButton_MouseHover);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(197, 281);
+            this.label1.BackColor = System.Drawing.Color.Snow;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(52, 223);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(125, 140);
+            this.label1.Size = new System.Drawing.Size(138, 168);
             this.label1.TabIndex = 5;
             this.label1.Text = "Square\r\nTriangle\r\nRectangle\r\nParallelogram\r\nTrapezoid\r\nPentagon\r\nCircle";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -120,33 +131,39 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(760, 287);
+            this.label2.BackColor = System.Drawing.Color.Snow;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(618, 235);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(142, 120);
+            this.label2.Size = new System.Drawing.Size(158, 144);
             this.label2.TabIndex = 6;
             this.label2.Text = "Cube \r\nCuboid\r\nCylinder\r\nSquare Pyramid\r\nSphere\r\nCone\r\n";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // mainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1082, 603);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(813, 496);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.headerPanel);
             this.Controls.Add(this.threeDimensionalButton);
             this.Controls.Add(this.twoDimensionalButton);
             this.Controls.Add(this.shapeChoosingLabel);
-            this.MaximumSize = new System.Drawing.Size(1100, 650);
-            this.MinimumSize = new System.Drawing.Size(1100, 650);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximumSize = new System.Drawing.Size(829, 535);
+            this.MinimumSize = new System.Drawing.Size(829, 535);
             this.Name = "mainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "MathProblemSolver";
             this.headerPanel.ResumeLayout(false);
-            this.headerPanel.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
