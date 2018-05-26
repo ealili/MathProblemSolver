6ee882c79c94dc156eb7d55aac6c92f890cae098
Enes Alili <enesalili00@gmail.com>
2018-05-27 00:14:00 +0200

    Design changes in ParallelogramForm

--- ParallelogramForm.Designer.cs
+++ ParallelogramForm.Designer.cs
@@ -39,15 +39,18 @@
             this.showPerimeter = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.getHeight = new System.Windows.Forms.TextBox();
+            this.titleLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // parallelogramBackButton
             // 
             this.parallelogramBackButton.AutoSize = true;
-            this.parallelogramBackButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.parallelogramBackButton.Location = new System.Drawing.Point(68, 149);
+            this.parallelogramBackButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.parallelogramBackButton.ForeColor = System.Drawing.Color.MediumSpringGreen;
+            this.parallelogramBackButton.Location = new System.Drawing.Point(12, 120);
+            this.parallelogramBackButton.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.parallelogramBackButton.Name = "parallelogramBackButton";
-            this.parallelogramBackButton.Size = new System.Drawing.Size(86, 29);
+            this.parallelogramBackButton.Size = new System.Drawing.Size(67, 24);
             this.parallelogramBackButton.TabIndex = 0;
             this.parallelogramBackButton.Text = "< Back";
             this.parallelogramBackButton.Click += new System.EventHandler(this.parallelogramBackButton_Click);
@@ -55,42 +58,61 @@
             // baseLabel
             // 
             this.baseLabel.AutoSize = true;
-            this.baseLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.baseLabel.Location = new System.Drawing.Point(226, 249);
+            this.baseLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.baseLabel.ForeColor = System.Drawing.Color.White;
+            this.baseLabel.Location = new System.Drawing.Point(170, 202);
+            this.baseLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.baseLabel.Name = "baseLabel";
-            this.baseLabel.Size = new System.Drawing.Size(168, 29);
+            this.baseLabel.Size = new System.Drawing.Size(114, 20);
             this.baseLabel.TabIndex = 1;
             this.baseLabel.Text = "Enter the base";
             // 
             // sideLabel
             // 
             this.sideLabel.AutoSize = true;
-            this.sideLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.sideLabel.Location = new System.Drawing.Point(226, 296);
+            this.sideLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sideLabel.ForeColor = System.Drawing.Color.White;
+            this.sideLabel.Location = new System.Drawing.Point(170, 240);
+            this.sideLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.sideLabel.Name = "sideLabel";
-            this.sideLabel.Size = new System.Drawing.Size(161, 29);
+            this.sideLabel.Size = new System.Drawing.Size(108, 20);
             this.sideLabel.TabIndex = 2;
             this.sideLabel.Text = "Enter the side";
             // 
             // getBase
             // 
-            this.getBase.Location = new System.Drawing.Point(482, 246);
+            this.getBase.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.getBase.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.getBase.ForeColor = System.Drawing.Color.MediumSpringGreen;
+            this.getBase.Location = new System.Drawing.Point(362, 200);
+            this.getBase.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.getBase.Name = "getBase";
-            this.getBase.Size = new System.Drawing.Size(100, 22);
+            this.getBase.Size = new System.Drawing.Size(100, 24);
             this.getBase.TabIndex = 3;
+            this.getBase.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // getSide
             // 
-            this.getSide.Location = new System.Drawing.Point(482, 296);
+            this.getSide.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.getSide.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.getSide.Location = new System.Drawing.Point(362, 240);
+            this.getSide.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.getSide.Name = "getSide";
-            this.getSide.Size = new System.Drawing.Size(100, 22);
+            this.getSide.Size = new System.Drawing.Size(100, 24);
             this.getSide.TabIndex = 4;
+            this.getSide.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // calculateButton
             // 
-            this.calculateButton.Location = new System.Drawing.Point(674, 259);
+            this.calculateButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.calculateButton.FlatAppearance.BorderColor = System.Drawing.Color.MediumSpringGreen;
+            this.calculateButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.calculateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calculateButton.ForeColor = System.Drawing.Color.White;
+            this.calculateButton.Location = new System.Drawing.Point(552, 232);
+            this.calculateButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.calculateButton.Name = "calculateButton";
-            this.calculateButton.Size = new System.Drawing.Size(88, 48);
+            this.calculateButton.Size = new System.Drawing.Size(120, 40);
             this.calculateButton.TabIndex = 5;
             this.calculateButton.Text = "Calculate";
             this.calculateButton.UseVisualStyleBackColor = true;
@@ -100,42 +122,69 @@
             // 
             this.showArea.AutoSize = true;
             this.showArea.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.showArea.Location = new System.Drawing.Point(229, 427);
+            this.showArea.ForeColor = System.Drawing.Color.MediumSpringGreen;
+            this.showArea.Location = new System.Drawing.Point(172, 347);
+            this.showArea.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.showArea.Name = "showArea";
-            this.showArea.Size = new System.Drawing.Size(0, 29);
+            this.showArea.Size = new System.Drawing.Size(0, 24);
             this.showArea.TabIndex = 6;
             // 
             // showPerimeter
             // 
             this.showPerimeter.AutoSize = true;
             this.showPerimeter.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.showPerimeter.Location = new System.Drawing.Point(229, 493);
+            this.showPerimeter.ForeColor = System.Drawing.Color.MediumSpringGreen;
+            this.showPerimeter.Location = new System.Drawing.Point(172, 401);
+            this.showPerimeter.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.showPerimeter.Name = "showPerimeter";
-            this.showPerimeter.Size = new System.Drawing.Size(0, 29);
+            this.showPerimeter.Size = new System.Drawing.Size(0, 24);
             this.showPerimeter.TabIndex = 7;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(228, 348);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(171, 283);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(181, 29);
+            this.label1.Size = new System.Drawing.Size(123, 20);
             this.label1.TabIndex = 8;
             this.label1.Text = "Enter the height";
             // 
             // getHeight
             // 
-            this.getHeight.Location = new System.Drawing.Point(482, 348);
+            this.getHeight.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.getHeight.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.getHeight.Location = new System.Drawing.Point(362, 283);
+            this.getHeight.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.getHeight.Name = "getHeight";
-            this.getHeight.Size = new System.Drawing.Size(100, 22);
+            this.getHeight.Size = new System.Drawing.Size(100, 24);
             this.getHeight.TabIndex = 9;
+            this.getHeight.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.titleLabel.Font = new System.Drawing.Font("Tempus Sans ITC", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.Color.White;
+            this.titleLabel.Location = new System.Drawing.Point(0, 0);
+            this.titleLabel.MaximumSize = new System.Drawing.Size(810, 100);
+            this.titleLabel.MinimumSize = new System.Drawing.Size(810, 100);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(810, 100);
+            this.titleLabel.TabIndex = 10;
+            this.titleLabel.Text = "Parallelogram";
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // ParallelogramForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1077, 596);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.ClientSize = new System.Drawing.Size(809, 491);
+            this.Controls.Add(this.titleLabel);
             this.Controls.Add(this.getHeight);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.showPerimeter);
@@ -148,8 +197,9 @@
             this.Controls.Add(this.parallelogramBackButton);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MaximumSize = new System.Drawing.Size(1099, 647);
-            this.MinimumSize = new System.Drawing.Size(1099, 647);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MaximumSize = new System.Drawing.Size(829, 534);
+            this.MinimumSize = new System.Drawing.Size(829, 534);
             this.Name = "ParallelogramForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "ParallelogramForm";
@@ -170,5 +220,6 @@
         private System.Windows.Forms.Label showPerimeter;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox getHeight;
+        private System.Windows.Forms.Label titleLabel;
     }
 }
