587c8d1561a2be28b33ef44244aa5a8d2939c153
Enes Alili <enesalili00@gmail.com>
2018-05-27 00:20:27 +0200

    Design changes in CubeForm

--- CubeForm.Designer.cs
+++ CubeForm.Designer.cs
@@ -35,16 +35,18 @@
             this.calculateButton = new System.Windows.Forms.Button();
             this.showVolume = new System.Windows.Forms.Label();
             this.showArea = new System.Windows.Forms.Label();
+            this.titleLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // cubeBackButton
             // 
             this.cubeBackButton.AutoSize = true;
             this.cubeBackButton.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.cubeBackButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cubeBackButton.Location = new System.Drawing.Point(66, 101);
+            this.cubeBackButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cubeBackButton.ForeColor = System.Drawing.Color.MediumSpringGreen;
+            this.cubeBackButton.Location = new System.Drawing.Point(12, 120);
             this.cubeBackButton.Name = "cubeBackButton";
-            this.cubeBackButton.Size = new System.Drawing.Size(73, 24);
+            this.cubeBackButton.Size = new System.Drawing.Size(67, 24);
             this.cubeBackButton.TabIndex = 0;
             this.cubeBackButton.Text = "< Back";
             this.cubeBackButton.Click += new System.EventHandler(this.cubeBackButton_Click);
@@ -53,6 +55,7 @@
             // 
             this.baseLabel.AutoSize = true;
             this.baseLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.baseLabel.ForeColor = System.Drawing.Color.White;
             this.baseLabel.Location = new System.Drawing.Point(158, 258);
             this.baseLabel.Name = "baseLabel";
             this.baseLabel.Size = new System.Drawing.Size(128, 20);
@@ -61,16 +64,26 @@
             // 
             // getBase
             // 
+            this.getBase.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.getBase.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.getBase.ForeColor = System.Drawing.Color.MediumSpringGreen;
             this.getBase.Location = new System.Drawing.Point(365, 257);
             this.getBase.Name = "getBase";
-            this.getBase.Size = new System.Drawing.Size(100, 20);
+            this.getBase.Size = new System.Drawing.Size(100, 24);
             this.getBase.TabIndex = 2;
+            this.getBase.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // calculateButton
             // 
-            this.calculateButton.Location = new System.Drawing.Point(580, 254);
+            this.calculateButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.calculateButton.FlatAppearance.BorderColor = System.Drawing.Color.MediumSpringGreen;
+            this.calculateButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.calculateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calculateButton.ForeColor = System.Drawing.Color.White;
+            this.calculateButton.Location = new System.Drawing.Point(565, 248);
+            this.calculateButton.MinimumSize = new System.Drawing.Size(120, 40);
             this.calculateButton.Name = "calculateButton";
-            this.calculateButton.Size = new System.Drawing.Size(75, 23);
+            this.calculateButton.Size = new System.Drawing.Size(120, 40);
             this.calculateButton.TabIndex = 3;
             this.calculateButton.Text = "Calculate";
             this.calculateButton.UseVisualStyleBackColor = true;
@@ -79,24 +92,45 @@
             // showVolume
             // 
             this.showVolume.AutoSize = true;
-            this.showVolume.Location = new System.Drawing.Point(165, 351);
+            this.showVolume.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.showVolume.ForeColor = System.Drawing.Color.MediumSpringGreen;
+            this.showVolume.Location = new System.Drawing.Point(159, 336);
             this.showVolume.Name = "showVolume";
-            this.showVolume.Size = new System.Drawing.Size(0, 13);
+            this.showVolume.Size = new System.Drawing.Size(0, 24);
             this.showVolume.TabIndex = 4;
             // 
             // showArea
             // 
             this.showArea.AutoSize = true;
-            this.showArea.Location = new System.Drawing.Point(165, 402);
+            this.showArea.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.showArea.ForeColor = System.Drawing.Color.MediumSpringGreen;
+            this.showArea.Location = new System.Drawing.Point(159, 412);
             this.showArea.Name = "showArea";
-            this.showArea.Size = new System.Drawing.Size(0, 13);
+            this.showArea.Size = new System.Drawing.Size(0, 24);
             this.showArea.TabIndex = 5;
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.titleLabel.Font = new System.Drawing.Font("Tempus Sans ITC", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.Color.White;
+            this.titleLabel.Location = new System.Drawing.Point(0, 0);
+            this.titleLabel.MaximumSize = new System.Drawing.Size(810, 100);
+            this.titleLabel.MinimumSize = new System.Drawing.Size(810, 100);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(810, 100);
+            this.titleLabel.TabIndex = 6;
+            this.titleLabel.Text = "Cube";
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // CubeForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
             this.ClientSize = new System.Drawing.Size(813, 496);
+            this.Controls.Add(this.titleLabel);
             this.Controls.Add(this.showArea);
             this.Controls.Add(this.showVolume);
             this.Controls.Add(this.calculateButton);
@@ -122,5 +156,6 @@
         private System.Windows.Forms.Button calculateButton;
         private System.Windows.Forms.Label showVolume;
         private System.Windows.Forms.Label showArea;
+        private System.Windows.Forms.Label titleLabel;
     }
 }
